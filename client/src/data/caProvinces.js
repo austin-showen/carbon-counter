7b85af6e145bebcc552c6bb6d2e6c9e74eae64c8
austin-showen <austin.showen@gmail.com>
2023-07-16 14:48:25 -0500

    add arrays of countries, states, and provinces

---
+++ client/src/data/caProvinces.js
@@ -0,0 +1,17 @@
+const provinceArray = [
+  { code: 'ab', name: 'Alberta' },
+  { code: 'bc', name: 'British Columbia' },
+  { code: 'mb', name: 'Manitoba' },
+  { code: 'nb', name: 'New Brunswick' },
+  { code: 'nl', name: 'Newfoundland and Labrador' },
+  { code: 'nt', name: 'Northwest Territories' },
+  { code: 'ns', name: 'Nova Scotia' },
+  { code: 'nu', name: 'Nunavut' },
+  { code: 'on', name: 'Ontario' },
+  { code: 'pe', name: 'Prince Edward Island' },
+  { code: 'qc', name: 'Quebec' },
+  { code: 'sk', name: 'Saskatchewan' },
+  { code: 'yt', name: 'Yukon' }
+]
+
+export default provinceArray
